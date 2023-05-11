c1f3fa330bde400360a7bad47a29716d27bb249f
LuckyStar04 <chenxin9204@gmail.com>
2023-05-11 22:38:25 +0800

    Bug fixed.

--- TechBlogCore.RestApi/Helpers/ExceptionMiddlewareExtensions.cs
+++ TechBlogCore.RestApi/Helpers/ExceptionMiddlewareExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
+using System.Text;
 
 namespace TechBlogCore.RestApi.Helpers
 {
@@ -12,15 +13,15 @@ namespace TechBlogCore.RestApi.Helpers
                 appError.Run(async context =>
                 {
                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    context.Response.ContentType = "text/plain";
+                    context.Response.ContentType = "text/plain; charset=utf-8";
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                     if (contextFeature != null)
                     {
                         logger.LogError($"- - - * * * 错误: {contextFeature.Error}");
                         if (contextFeature.Error is MessageException)
-                            await context.Response.WriteAsync(contextFeature.Error.Message);
+                            await context.Response.WriteAsync(contextFeature.Error.Message, Encoding.UTF8);
                         else
-                            await context.Response.WriteAsync("内部服务错误");
+                            await context.Response.WriteAsync("内部服务错误", Encoding.UTF8);
                     }
                 });
             });
